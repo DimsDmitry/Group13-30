d4df11df4b496479bfc4938320cafe8a80e47a09
Dmitriy <dmitriy250300@mail.ru>
2023-04-19 16:03:25 +0300

    19.04.2023

--- m3_pandas/lesson3/data_cleaning.py
+++ m3_pandas/lesson3/data_cleaning.py
@@ -22,12 +22,16 @@ def set_size(size):
         return float(size[:-1]) / 1024
     return -1
 
+
 df['Size'] = df['Size'].apply(set_size)
+
 
 print(df['Size'].value_counts())
 
 # Чему равен максимальный размер ('Size') приложений из категории ('Category') 'TOOLS'?
 print(df[df['Category'] == 'TOOLS']['Size'].max())
+
+print(100 * '-')
 
 
 # Бонусные задания
@@ -35,11 +39,30 @@ print(df[df['Category'] == 'TOOLS']['Size'].max())
 # В записи количества установок ('Installs') знак "+" необходимо игнорировать.
 # Т.е. если в датасете количество установок равно 1,000,000+, то необходимо изменить значение на 1000000
 
+def set_installs(installs):
+    if installs == '0':
+        return 0
+    return int(installs[:-1].replace(',', ''))
+
+
+df['Installs'] = df['Installs'].apply(set_installs)
+print(df['Installs'])
+
+print(100 * '=')
 # Сгруппируй данные по категории ('Category') и целевой аудитории ('Content Rating') любым удобным для тебя способом,
 # посчитай среднее количество установок ('Installs') в каждой группе. Результат округли до десятых.
 # В полученной таблице найди ячейку с самым большим значением.
 # К какой возрастной группе и типу приложений относятся данные из этой ячейки?
+print(round(df.pivot_table(index = 'Content Rating', columns = 'Type', values = 'Installs', aggfunc = 'mean')), 1)
+
 
 # У какого приложения не указан тип ('Type')? Какой тип там необходимо записать в зависимости от цены?
+print('*' * 20)
+print(df[pd.isnull(df['Type'])])
+# Чтобы увидеть все столбцы вместо многоточия, можно применить iloc[0].
+print(df[pd.isnull(df['Type'])].iloc[0])
+df['Type'].fillna('Free', inplace=True)
+
 
 # Выведи информацию о всем DataFrame, чтобы убедиться, что очистка прошла успешно
+print(df.info())
