d11fe98469bba2fe79ea287e03ee212058f152ed
Dmitriy <dmitriy250300@mail.ru>
2023-05-11 09:04:06 +0300

    11.05.2023

--- m4/lesson1/all_done.py
+++ m4/lesson1/all_done.py
@@ -3,10 +3,10 @@ import pandas as pd
 
 # Шаг 1. Загрузка и очистка данных
 df = pd.read_csv('titanic.csv')
-df.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis = 1, inplace = True)
+df.drop(['PassengerId', 'Name', 'Ticket', 'Cabin'], axis=1, inplace=True)
 df[list(pd.get_dummies(df['Embarked']).columns)] = pd.get_dummies(df['Embarked'])
-df['Embarked'].fillna('S', inplace = True)
-df.drop('Embarked', axis = 1, inplace = True)
+df['Embarked'].fillna('S', inplace=True)
+df.drop('Embarked', axis=1, inplace=True)
 
 
 age_1 = df[df['Pclass'] == 1]['Age'].median()
@@ -15,13 +15,13 @@ age_3 = df[df['Pclass'] == 3]['Age'].median()
 
 
 def fill_age(row):
-   if pd.isnull(row['Age']):
-       if row['Pclass'] == 1:
-           return age_1
-       if row['Pclass'] == 2:
-           return age_2
-       return age_3
-   return row['Age']
+    if pd.isnull(row['Age']):
+        if row['Pclass'] == 1:
+            return age_1
+        if row['Pclass'] == 2:
+            return age_2
+        return age_3
+    return row['Age']
 
 
 df['Age'] = df.apply(fill_age, axis = 1)
@@ -43,9 +43,9 @@ from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix, accuracy_score
 
 
-X = df.drop('Survived', axis = 1)
+X = df.drop('Survived', axis=1)
 y = df['Survived']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25)
 
 
 sc = StandardScaler()
@@ -53,7 +53,7 @@ X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 
-classifier = KNeighborsClassifier(n_neighbors = 5)
+classifier = KNeighborsClassifier(n_neighbors=5)
 classifier.fit(X_train, y_train)
 
 
